f03d7f446e9d3718f54f34cfc559b18f604908ab
Mishrask637 <Mishrask637@gmail.com>
2024-04-21 17:48:17 +0530

    Updated Scripts

--- src/test/java/runner/CurrencyConverterRunner.java
+++ src/test/java/runner/CurrencyConverterRunner.java
@@ -3,8 +3,8 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features/CurrencyConverter.feature", glue = { "stepdefinitions",
-		"hooks" }, plugin = { "pretty", "json:target/cucumber-reports/json/cucumber-json-report.json",
+@CucumberOptions(features = "src/test/resources/features/CurrencyConverter.feature", glue = {
+		"stepdefinitions" }, plugin = { "pretty", "json:target/cucumber-reports/json/cucumber-json-report.json",
 				"html:target/cucumber-reports/html/currencyConverter.html" }, dryRun = false, monochrome = true)
 public class CurrencyConverterRunner extends AbstractTestNGCucumberTests {
 }
